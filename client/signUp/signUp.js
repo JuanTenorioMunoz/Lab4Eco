d34daa068371f0b4cd0d1c0c2089e3092d6cbd85
Juan Camilo Tenorio Munoz <1193225773@u.icesi.edu.co>
2024-08-23 08:52:25 -0500

    working_ServerInteractions

---
+++ client/signUp/signUp.js
@@ -0,0 +1,22 @@
+const userName = document.getElementById("userName");
+const named = document.getElementById("name");
+const password = document.getElementById("password");
+
+
+const register = () => {
+
+    const user = {
+        userName: userName,
+        name: named,
+        password: password,
+    }
+
+    try {
+        const response = await fetch
+    } catch (error) {
+        
+    }
+}
+
+
+const submit = document.getElementById("submit").addEventListener("click", register)
