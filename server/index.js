d3fe3c374ae112058844f50ee417ee99588a60c8
unknown <tenorio2003@gmail.com>
2024-09-18 10:47:39 -0500

    fix_screenNav

--- server/index.js
+++ server/index.js
@@ -4,48 +4,16 @@ const http = require('http');
 const socketIo = require('socket.io');
 
 const app = express();
-app.use(express.json()); // utility to process JSON in requests
-app.use(cors()); // utility to allow clients to make requests from other hosts or ips
-
-const db = {
-  players: [],
-};
-
-app.get("/users", (request, response) => {
-  response.send(db);
-});
-
-app.get("/user", (request, response) => {
-  const userName = request.query.name; 
-  const user = db.players.find(player => player.name === userName);
-
-  if (user) {
-    response.status(200).json(user);
-  } else {
-    response.status(404).send("User not found"); 
-  }
-});
-
-app.post("/user", (request, response) => {
-  const { body } = request;
-  db.players.push(body);
-  response.status(201).send(body); 
-});
-
-app.patch("/user", (req, res) => {
-  const userName = req.query.name; 
-  const user = db.players.find(player => player.name === userName); 
-
-  if (user) {
-    Object.assign(user, req.body); 
-    res.status(200).json(user); 
-  } else {
-    res.status(404).send("User not found");
-  }
-});
+app.use(express.json()); // Utility to process JSON in requests
+app.use(cors()); // Utility to allow clients to make requests from other hosts or IPs
 
 // Create an HTTP server using the Express app
 const server = http.createServer(app);
+
+const db = {
+  drivers: [],
+  passager: [],
+};
 
 // Initialize socket.io on the same server
 const io = socketIo(server, {
